51424858974e9acb6c8ccd4119a48369cc1047a9
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2014-05-03 01:14:36 +0200

    create translate frequency

--- pension_functions.py
+++ pension_functions.py
@@ -9,38 +9,25 @@ chomage=2
 avpf = 8
 id_test = 21310 # 28332 #1882 #1851 #, 18255
 
-def workstate_selection(table, code_regime=None, input_step='month', output_step='month', option='dummy'):
-    ''' Input : monthly or yearly-table (lines: indiv, col: dates 'yyyymm') 
-    Output : (0/1)-pandas matrix with 1 = indiv has worked at least one month during the civil year in this regime if yearly-table'''
-    if not code_regime:
-        raise Exception("Indiquer le code identifiant du régime")
-        
-    if input_step == output_step:
-        selection = table.isin(code_regime).astype(int)
-        table_code = table
-    else: 
-        year_start, year_end = interval_years(table)
-        selected_dates = [100*year + 1 for year in range(year_start, year_end)]
-        if output_step == 'month':
-            selected_dates = [100*year + month + 1 for year in range(year_start, year_end) for month in range(12)]
-        selection = pd.DataFrame(index = table.index, columns = selected_dates)
-        table_code = pd.DataFrame(index = table.index, columns = selected_dates)
-        for year in range(year_start, year_end) :
-            code_selection = table[year * 100 + 1].isin(code_regime)
-            table_code[year * 100 + 1] = table[year * 100 + 1]
-            if input_step == 'month' and output_step == 'year': 
-                for i in range(2,13):
-                    date = year * 100 + i
-                    code_selection = code_selection  * table[date].isin(code_regime)
-                selection[year * 100 + 1] = code_selection.astype(int)
-            elif input_step == 'year' and output_step == 'month':
-                for i in range(1,13):
-                    date = year * 100 + i
-                    selection[date] = code_selection.astype(int) 
-                    table_code[date] = table[year * 100 + 1]             
-    if option == 'code':
-        selection = table_code * selection
-    return selection
+def translate_frequency(table, input_frequency='month', output_frequency='month', method='useless'):
+    '''method should eventually control how to switch from month based table to year based table
+        so far we assume year is True if January is True '''
+    assert all( table.dtypes == 'bool')
+    if input_frequency == output_frequency:
+        return table
+    if output_frequency == 'year': # if True here, input_frequency=='month'
+        detected_years = set([date // 100 for date in table.columns])
+        output_dates = [100*x + 1 for x in detected_years]
+        #here we could do more complex
+        output_table = table.loc[:, output_dates]
+        return output_table
+    if output_frequency == 'month': # if True here, input_frequency=='year'
+        output_dates = [x + k for x in table.columns for k in range(12)]
+        #here we could do more complex
+        output_table = pd.DataFrame(index=table.index, columns=output_dates)
+        output_table.loc[:,table.columns] = table
+        output_table.fillna(method='ffill', axis=1, inplace=True)
+        return output_table        
     
 def select_unemployment(data, code_regime, option='dummy'):
     ''' Ne conserve que les périodes de chomage succédant directement à une période de cotisation au régime
@@ -75,7 +62,8 @@ def unemployment_trimesters(table, code_regime = None, input_step = 'month', out
             assert step == 'year'
             return 4 * nb_trim, unemp_trim
         
-    table = workstate_selection(table, code_regime = code_regime + [chomage], input_step = input_step, output_step = input_step, option = 'code')
+    table = table.isin(code_regime + [chomage])
+    table = translate_frequency(table, input_frequency=input_step, output_frequency=input_step)
     nb_trim_chom, unemp_trim = _calculate_trim_unemployment(table, step = input_step, code_regime = code_regime)
     if output == 'table_unemployement':
         return nb_trim_chom, unemp_trim
