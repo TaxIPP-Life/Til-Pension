ff81eabf86c8dd78807ab88fc9cb4ab8f8822427
LouisePaulDelvaux <louise.paul.delvaux@gmail.com>
2014-05-06 18:29:53 +0200

    trim_service and build_age_ref in FP

--- Regimes/Fonction_publique.py
+++ Regimes/Fonction_publique.py
@@ -11,7 +11,7 @@ import os
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.sys.path.insert(0,parentdir) 
 from SimulPension import PensionSimulation
-from utils import sum_by_years, substract_months, valbytranches, table_selected_dates, build_long_values, translate_frequency
+from utils import _isin, build_long_values, sum_by_years, substract_months, translate_frequency, valbytranches, table_selected_dates
 from pension_functions import calculate_SAM, sal_to_trimcot, unemployment_trimesters
 code_avpf = 8
 code_chomage = 5
@@ -25,6 +25,8 @@ class FonctionPublique(PensionSimulation):
         PensionSimulation.__init__(self)
         self.regime = 'FP'
         self.code_regime = [5,6]
+        self.code_sedentaire = 6
+        self.code_actif = 5
         
         self._P = param_regime
         self._Pcom = param_common
@@ -36,20 +38,38 @@ class FonctionPublique(PensionSimulation):
         self.info_child_mother = None
         self.info_child_father = None
         self.time_step = None
-
+        
         
     def trim_service(self):
         ''' Cette fonction pertmet de calculer la durée de service dans FP
         TODO: gérer la comptabilisation des temps partiels quand variable présente'''
-        wk_selection = self.workstate.isin(self.code_regime)
-        wk_selection = translate_frequency(wk_selection, input_frequency=self.time_step, output_step='month')
+        wk_selection = _isin(self.workstate,self.code_regime)
+        wk_selection = translate_frequency(wk_selection, input_frequency=self.time_step, output_frequency='month')
+        wk_selection_actif = _isin(self.workstate,self.code_actif)
+        wk_selection_actif = translate_frequency(wk_selection_actif, input_frequency=self.time_step, output_frequency='month')
         # TODO: condition not assuming sali is in year
         sali = translate_frequency(self.sali, input_frequency='year', output_frequency='month')
         sali = np.around(np.divide(sali, 12), decimals=3)
-     
         sal_selection = wk_selection*sali 
-        trim_service = np.divide(wk_selection.sum(axis=1), 4)
+        trim_service = np.divide(wk_selection.sum(axis=1), 4).astype(int)
+        trim_actif = np.divide(wk_selection_actif.sum(axis=1), 4).astype(int)
         self.sal_FP = sal_selection
-        return trim_service
+        return trim_service, trim_actif
  
-    
+    def build_age_ref(self, trim_actif):
+        P = self._P
+        # age_min = age_min_actif pour les fonctionnaires actif en fin de carrières ou carrière mixte ayant une durée de service actif suffisante
+        age_min_s = valbytranches(P.sedentaire.age_min, self.info_ind)
+        age_min_a = valbytranches(P.actif.age_min, self.info_ind)
+        age_min = age_min_a*(trim_actif >= P.actif.N_min) + age_min_s*(trim_actif < P.actif.N_min)
+        self._P.age_min_vec = age_min
+        
+        # age limite = age limite associée à la catégorie de l’emploi exercé en dernier lieu
+        last_wk = self.workstate.iloc[:,-1]
+        fp = last_wk.isin(self.code_regime)
+        actif = (last_wk == self.code_actif)
+        sedentaire = fp*(1 - actif)
+        age_max_s = valbytranches(P.sedentaire.age_max, self.info_ind)
+        age_max_a = valbytranches(P.actif.age_max, self.info_ind)
+        age_max = actif*age_max_a + sedentaire*age_max_s
+        self._P.age_max_vec = age_max
