7ff802355185b151a475216283d42b8a49054aa5
LouisePaulDelvaux <louise.paul.delvaux@gmail.com>
2014-05-15 17:59:35 +0200

    Add frequency as initial attribute of TimeArray

--- time_array.py
+++ time_array.py
@@ -2,11 +2,26 @@
 import numpy as np
 from pandas import DataFrame
 
+def determine_frequency(dates):
+    if (np.array(dates) % 100 == 1).all() :
+        frequency = 'year' 
+    else: 
+        frequency = 'month' 
+    return frequency
+
 class TimeArray(object):
     
     def __init__(self, array, dates):
         self.array = array
         self.dates = dates
+        self.frequency = determine_frequency(dates)
+    
+    def copy(self):
+        return TimeArray(self.array.copy(), self.dates)
+    
+    def isin(self, code):
+        array_selection = np.in1d(self.array.copy(), code).reshape(self.array.shape)
+        return TimeArray(array_selection, self.dates)
         
     def selected_dates(self, first, last, date_type='year', inplace=False):
         ''' La table d'input dont les colonnes sont des dates est renvoyées amputée 
@@ -32,46 +47,41 @@ class TimeArray(object):
         else:
             return TimeArray(array[:,array_dates], dates)
     
-    def _copy(self):
-        return TimeArray(self.array.copy(), self.dates)
-    
-    def _isin(self, code):
-        array_selection = np.in1d(self.array.copy(), code).reshape(self.array.shape)
-        return TimeArray(array_selection, self.dates)
-    
     def translate_frequency(self, output_frequency='month', method=None, data_type='numpy', inplace=False):
         '''method should eventually control how to switch from month based table to year based table
             so far we assume year is True if January is True 
             idea : format_table as an argument instead of testing with isinstance
             '''
-        if (np.array(self.dates) % 100 == 1).all() :
-            input_frequency = 'year' 
-        else : 
-            input_frequency = 'month' 
+        array = self.array.copy()
+        input_frequency = determine_frequency(self.dates)
         if input_frequency == output_frequency:
-            return (self.array, self.dates) 
+            if inplace == True:
+                return 'rien'
+            else:
+                return (array, self.dates)
         if data_type == 'numpy':
             if output_frequency == 'year': # if True here, input_frequency=='month'
                 assert len(self.dates) % 12 == 0 #TODO: to remove eventually
                 nb_years = len(self.dates) // 12
                 output_dates = [date for date in self.dates if date % 100 == 1]
-                output_dates_ix = self.dates.index(output_dates)
+                output_dates_ix = [self.dates.index(output_date) for output_date in output_dates]
                 #here we could do more complex
                 if method is None: #first month of each year
                     output = self.array[:, output_dates_ix]
                 if method is 'sum':
-                    output = self.array[:, output_dates_ix].copy()
+                    output = array[:, output_dates_ix]
                     for month in range(1,12):
                         month_to_add = [year*12 + month for year in xrange(nb_years)]
-                        output += self.array[:, month_to_add]
+                        output += array[:, month_to_add]
             elif output_frequency == 'month': # if True here, input_frequency=='year'
                 output_dates = [year + month for year in self.dates for month in range(12)]
-                output = np.repeat(self.array, 12, axis=1)
+                output = np.repeat(array, 12, axis=1)
                 if method == 'divide':
                     output = np.around(np.divide(output, 12), decimals=3)
             if inplace == True:
                 self.array = output
                 self.dates = output_dates
+                self.frequency = output_frequency
             else:
                 return (output, output_dates)
                 
