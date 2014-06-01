a2a9daf55f2186596eccc6bd44c46480edeaf116
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2014-06-01 22:24:37 +0200

    sandbox

---
+++ sandbox/utils_compar.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 1 juin 2014
+
+@author: alexis
+'''
+
+from pandas import Series
+from numpy import zeros
+
+def calculate_age(birth_date, date):
+    ''' calculate age at date thanks birthdate '''
+    def _age(birthdate):
+        try: 
+            birthday = birthdate.replace(year=date.year)
+        except ValueError: # raised when birth date is February 29 and the current year is not a leap year
+            birthday = birthdate.replace(year=date.year, month=birthdate.month+1, day=1)
+        if birthday > date:
+            return date.year - birthdate.year - 1
+        else:
+            return date.year - birthdate.year
+    return birth_date.apply(_age)
+
+def count_enf_pac(info_child, index):
+    info_child['enf_pac'] = ( info_child['age_enf'] <= 18)*( info_child['age_enf'] >= 0 )*info_child['nb_enf']
+    info = info_child.groupby(['id_parent'])['enf_pac'].sum().reset_index()
+    info.columns = ['id_parent', 'nb_pac']
+    info.index = info['id_parent']
+    nb_pac= Series(zeros(len(index)), index=index)
+    nb_pac += info['nb_pac']
+    return nb_pac.fillna(0)
+
+def count_enf_born(info_child, index):
+    info_child['enf_born'] =  ( info_child['age_enf'] >= 0 )*info_child['nb_enf']
+    info = info_child.groupby(['id_parent'])['enf_born'].sum().reset_index()
+    info.columns = ['id_parent', 'nb_born']
+    info.index = info['id_parent']
+    nb_born= Series(zeros(len(index)), index=index)
+    nb_born += info['nb_born']
+    return nb_born.fillna(0)
