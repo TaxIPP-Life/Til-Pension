7489bc2e86c2b08fad5286f3b104f1271715faf3
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2014-06-16 22:56:28 +0200

    add possibility to load_param directly

--- pension_legislation.py
+++ pension_legislation.py
@@ -186,4 +186,25 @@ class PensionLegislation(object):
             salmin.loc[salmin['year'] == year, 'sal'] = self.param.param.prive.RG.nb_h*smic_long[smic_year[0]]
         return array(salmin['sal'])  
 
-        
+if __name__ == '__main__':
+    from pandas import DataFrame
+    from pension_data import PensionData
+    import datetime
+    
+    data = DataFrame()
+    table = array([ (186L, 1941.0, 2.0, 1.0, datetime.date(1941, 1, 1), 186L, 756.0, 0.0, 2.0, datetime.date(2060, 1, 1)),
+       (376L, 1941.0, 1.0, 1.0, datetime.date(1941, 1, 1), 376L, 756.0, 0.0, 1.0, datetime.date(2060, 1, 1)),
+       (833L, 1941.0, 3.0, 0.0, datetime.date(1941, 1, 1), 833L, 756.0, 0.0, 3.0, datetime.date(2060, 1, 1)),
+       (834L, 1941.0, 3.0, 1.0, datetime.date(1941, 1, 1), 834L, 756.0, 0.0, 3.0, datetime.date(2060, 1, 1)),
+       (956L, 1941.0, 0.0, 0.0, datetime.date(1941, 1, 1), 956L, 756.0, 0.0, 0.0, datetime.date(2060, 1, 1))], 
+      dtype=[('index', '<i8'), ('t_naiss', '<f8'), ('n_enf', '<f8'), ('sexe', '<f8'), ('naiss', 'O'), ('id', '<i8'), ('agem', '<f8'), ('nb_pac', '<f8'), ('nb_born', '<f8'), ('date_liquidation', 'O')])
+    info_ind = DataFrame(table)
+    sali = DataFrame(0, index=info_ind.index, columns=[201301,201401])
+    data = PensionData.from_arrays(sali, sali, info_ind)
+    
+    test = PensionParam(2005, data)
+    print test.param.prive.RG.prorat
+    import pdb
+    pdb.set_trace()
+
+    
