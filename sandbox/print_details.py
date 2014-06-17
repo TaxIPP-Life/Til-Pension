4bc252377cfdb978ab142137585ce57beef26bb3
LouisePaulDelvaux <louise.paul.delvaux@gmail.com>
2014-06-17 17:47:12 +0200

    Cleaner print_details

--- sandbox/print_details.py
+++ sandbox/print_details.py
@@ -12,16 +12,6 @@ def print_details(data, yearsim, loggerlevel):
     simul_til.profile_evaluate(to_check=False, logger=loggerlevel)
     
 if __name__ == '__main__':    
-
-    var_to_check_montant = [ u'pension_RG', u'salref_RG', u'DA_RG', u'DA_RSI', 
-                            u'nb_points_arrco', u'nb_points_agirc', u'pension_arrco', u'pension_agirc',
-                            u'DA_FP', u'pension_FP',
-                            u'n_trim_RG', 'N_CP_RG', 'n_trim_FP'
-                            ] 
-    var_to_check_taux = [u'taux_RG', u'surcote_RG', u'decote_RG', u'CP_RG',
-                         u'taux_FP'
-                          ]
-    threshold = {'montant' : 1, 'taux' : 0.05}
     import logging
     import sys
     yearsim = 2004
@@ -32,4 +22,4 @@ if __name__ == '__main__':
                                      selection_id = [7338])
     
     loggerlevel = {'evaluate': 'info', 'decote':'error', 'surcote': 'error'}
-    print_details(data_to_print, 2004, loggerlevel)
+    print_details(data_to_print, yearsim, loggerlevel)
