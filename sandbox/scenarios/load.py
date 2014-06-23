d4e0e4385f89764e5f8543cdd514648ac0014211
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2014-06-24 00:10:15 +0200

    further in load scenario

--- sandbox/scenarios/load.py
+++ sandbox/scenarios/load.py
@@ -15,11 +15,12 @@ dates = [100*x + 1 for x in range(1969,2010)]
 
 sali = zeros((nb_scenarios,len(dates)))
 workstate = zeros((nb_scenarios,len(dates)))
-info_ind = DataFrame(index=range(nb_scenarios), columns=['agem','naiss','sexe','nb_born','nb_pac'])
+info_ind = DataFrame(index=range(nb_scenarios), columns=['agem','naiss','sexe','nb_enf','nb_pac','nb_enf_FP','tauxprime'])
 
 def load_case(i):
     file = data_scenarios_path + 'data' +str(i) + '.csv'
     data = read_table(file, sep=',')
+    # print data.loc[40,'agedep'] c'est toujours 55 ! Inutile
     assert all((data['agirc']==1) == ((data['I_cnav']==1) & (data['agirc']==1)))
     workstate = 3*(data['I_cnav']==1) + (data['agirc']==1) + 7*(data['I_rsi']==1) + 5*(data['I_pub']==1) + (data['inactif']==1)
     #TODO: tenir compte de l'AVPF
@@ -36,17 +37,28 @@ agem = (2009-1954 + 0.5)*12
 naiss = dt.date(1954, 6, 1)
 info_ind['agem'] = agem
 info_ind['naiss'] = naiss
+info_ind['tauxprime'] = 0
 for i in range(nb_scenarios):
     work_i, sali_i, sexe, nb_enf = load_case(i+1) # Attention déclage dans la numérotaiton qui ne commence pas à zeros
     sali[i,:] = sali_i
     workstate[i,:] = work_i
-    info_ind.loc[i,['sexe','nb_born','nb_pac']] = [sexe, nb_enf, nb_enf]
+    info_ind.loc[i,['sexe','nb_enf','nb_pac','nb_enf_FP']] = [sexe, nb_enf, nb_enf, nb_enf]
 
 #TODO: know why nbenf is often NaN and not 0.
 info_ind.fillna(0, inplace=True)
 data = PensionData.from_arrays(workstate, sali, info_ind, dates)
 
+
+from pension_legislation import PensionParam, PensionLegislation
+from simulation import PensionSimulation
+
+param = PensionParam(201001, data)
+legislation = PensionLegislation(param)
+simulation = PensionSimulation(data, legislation)
+trim = simulation.evaluate(output='trimesters_wages')
+result_til_year = simulation.evaluate(to_check=True)
+
 pdb.set_trace()
-
-
+trim['FP']['trimesters']
+trim['FP']['maj']
 
