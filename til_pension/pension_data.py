3bc1f44e7def456a8407b06e79cdc671cd6747fd
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2014-07-08 13:45:16 +0200

    Path correction

--- til_pension/pension_data.py
+++ til_pension/pension_data.py
@@ -2,31 +2,29 @@
 
 from numpy import array, ndarray
 from pandas import DataFrame
-from time_array import TimeArray
+from til_pension.time_array import TimeArray
 from datetil import DateTil
 
-compare_destinie = True 
+compare_destinie = True
 
 class PensionData(object):
     '''
     Class à envoyer à Simulation de Til-pension
     '''
-    def __init__(self, workstate, sali, info_ind): 
+    def __init__(self, workstate, sali, info_ind):
         assert isinstance(workstate, TimeArray)
         assert isinstance(sali, TimeArray)
         self.workstate = workstate
         self.sali = sali
         self.info_ind = info_ind
-        
-        
         assert workstate.dates == sali.dates
         dates = sali.dates
         assert sorted(dates) == dates
-        self.last_date = None #intialisation for assertion in set_dates
+        self.last_date = None  # intialisation for assertion in set_dates
         self.set_dates(dates)
         if 'date_liquidation' not in info_ind.columns:
             self.info_ind['date_liquidation'] = self.last_date.datetime
-  
+
     def set_dates(self, dates):
         self.dates = dates
         # on ne change pas last_date pour ne pas avoir d'incohérence avec date_liquidation
@@ -34,7 +32,7 @@ class PensionData(object):
             raise Exception("Impossible to change last_date of a data_frame")
         self.last_date = DateTil(dates[-1])
         self.first_date = DateTil(dates[0])
-        
+
     def selected_dates(self, first=None, last=None, date_type='year', inplace=False):
         ''' cf TimeArray '''
         if inplace:
@@ -45,7 +43,7 @@ class PensionData(object):
             wk = self.workstate.selected_dates(first, last, date_type)
             sal = self.sali.selected_dates(first, last, date_type)
             return PensionData(wk, sal, self.info_ind)
-        
+
     def selected_regime(self, code_regime):
         ''' Cette fonction renvoie une copie corrigée de l'objet PensionData dans lequel :
         - tous les workstate de data.workstate qui ne figurent pas dans code_regime sont remplacés par 0
@@ -55,9 +53,9 @@ class PensionData(object):
         sal = self.sali.copy()
         work = self.workstate.copy()
         wk_regime = TimeArray(wk_selection*work, sal.dates, name='workstate_regime')
-        sal_regime = TimeArray(wk_selection*sal, sal.dates, name='sali_regime') 
+        sal_regime = TimeArray(wk_selection*sal, sal.dates, name='sali_regime')
         return PensionData(wk_regime, sal_regime, self.info_ind)
-        
+
     def translate_frequency(self, output_frequency='month', method=None, inplace=False):
         ''' cf TimeArray '''
         if inplace:
@@ -87,19 +85,18 @@ class PensionData(object):
                 decal = idx[~idx.isin(sal)][0] - sal[~sal.isin(idx)][0]
                 import pdb
                 pdb.set_trace()
-        
+
             #TODO: should be done before
             assert sali.columns.tolist() == workstate.columns.tolist()
             assert sali.columns.tolist() == (sorted(sali.columns))
             dates = sali.columns.tolist()
             sali = array(sali)
             workstate = array(workstate)
-            
+
         if isinstance(sali, ndarray):
             assert isinstance(workstate, ndarray)
             sali = TimeArray(sali, dates, name='sali')
             workstate = TimeArray(workstate, dates, name='workstate')
-        
-        assert info_ind['sexe'].isin([0,1]).all() 
+
+        assert info_ind['sexe'].isin([0,1]).all()
         return PensionData(workstate, sali, info_ind)
-        
--- til_pension/pension_legislation.py
+++ til_pension/pension_legislation.py
@@ -17,33 +17,33 @@ from Regimes.Regimes_complementaires_prive import AGIRC, ARRCO
 from Regimes.Regimes_prives import RegimeGeneral, RegimeSocialIndependants
 from regime import compare_destinie
 
-def build_long_values(param_long, first, last, time_scale='year'):   
-    ''' Cette fonction permet de traduire les paramètres longitudinaux en vecteur numpy 
+def build_long_values(param_long, first, last, time_scale='year'):
+    ''' Cette fonction permet de traduire les paramètres longitudinaux en vecteur numpy
     comportant une valeur par année comprise en first_year et last_year '''
     #TODO: Idea : return a TimeArray and use select if needed
     param_dates = sorted(param_long.keys())
     def _convert_date(x):
         date = dt.datetime.strptime(x, "%Y-%m-%d")
         return 100*date.year + date.month
-    #TODO: convert here all param in dates   
+    #TODO: convert here all param in dates
     param_dates_liam = [_convert_date(x) for x in param_dates]
     param_dates_liam += [210001]
 
     if time_scale=='year':
         list_dates = [100*x + 1  for x in range(first, last)]
-    else: 
+    else:
         #TODO: create a function...
         raise Exception("Not implemented yet for time_scale not year")
-    
+
     output = []
     k = 0
     for date in list_dates:
         while param_dates_liam[k+1] <= date:
             k += 1
-        output += [param_long[param_dates[k]]]  
+        output += [param_long[param_dates[k]]]
     return output
-        
-def scales_long_baremes(baremes, scales):   
+
+def scales_long_baremes(baremes, scales):
     ''' Cette fonction permet de traduire les barèmes longitudinaux en dictionnaire de bareme
     comportant un barème par année comprise en first_year et last_year'''
     from Param.Scales import scaleBaremes
@@ -53,13 +53,13 @@ def scales_long_baremes(baremes, scales):
     return baremes
 
 class PensionParam(object):
-    
+
     def __init__(self, dateleg, data):
         #TODO: use all attrbutes except data in a PensionParam class
-        # example: 
-        #     duration = data.last_year - data.first_year 
+        # example:
+        #     duration = data.last_year - data.first_year
         #     self.param = PensionParam.builder(dateleg, data.info_ind, duration)
-        #  or, by anticipation: 
+        #  or, by anticipation:
         #     self.param = PensionParam.builder(dateleg, data.info_ind, duration, method)
         #  where method give how to shift legislation from on year to an other, constant_year, constant_sequence, etc?
         self.date = DateTil(dateleg)
@@ -70,8 +70,8 @@ class PensionParam(object):
 #             ''' should run after having a data '''
         assert data is not None
         path_pension = os.path.dirname(os.path.abspath(__file__))
-        param_file = path_pension + '\\France\\param.xml'
-        
+        param_file = os.path.join(path_pension, 'France', 'param.xml')
+
         ''' It's a simplification of an (old) openfisca program '''
         legislation_tree = ElementTree.parse(param_file)
         legislation_xml_json = conv.check(legislationsxml.xml_legislation_to_json)(legislation_tree.getroot(),
@@ -79,34 +79,34 @@ class PensionParam(object):
         #legislation_xml_json, _ = legislationsxml.validate_node_xml_json(legislation_xml_json,
         #                                                                 state = conv.default_state)
         _, legislation_json = legislationsxml.transform_node_xml_json_to_json(legislation_xml_json)
-        
+
         dated_legislation_json = legislations.generate_dated_legislation_json(legislation_json, self.date.datetime)
         compact_legislation = legislations.compact_dated_node_json(dated_legislation_json, data.info_ind) #here is where data is needed
         self.P = compact_legislation
-        
+
         long_dated_legislation_json = legislations.generate_long_legislation_json(legislation_json, self.date.datetime)
         compact_legislation_long = legislations.compact_long_dated_node_json(long_dated_legislation_json)
         self.P_longit = compact_legislation_long
 
-        #Travail sur Salref 
+        #Travail sur Salref
         #
         # Salref = salaire trimestriel de référence minimum pour le régime général
         #
         # Note: il existe deux opinions différentes acceptables mais non compatible sur la construction du salref
-        # - la première c'est de considérer que le salaire de référence de chaque année est donné directement par la 
+        # - la première c'est de considérer que le salaire de référence de chaque année est donné directement par la
         #  legisaltion et que même si c'est écrit comme étant égal à d'autres paramètres de la législation, c'est un
         #  élément indépendant avec son propre code
-        # - la seconde, consiste à voir l'étape de détermination du salaire de référence comme une étape de la 
-        #  législation; le fait d'indexer sur tel ou tel indice n'est pas anodin. 
+        # - la seconde, consiste à voir l'étape de détermination du salaire de référence comme une étape de la
+        #  législation; le fait d'indexer sur tel ou tel indice n'est pas anodin.
         #
-        # Les deux sont valables, ici, on s'appuie sur la première, on ne fait que recalculer le niveau du salref 
-        # à partir de sa définition. On considère en particulier que l'indexation sur le SMIC a lieu en 1972 et 
+        # Les deux sont valables, ici, on s'appuie sur la première, on ne fait que recalculer le niveau du salref
+        # à partir de sa définition. On considère en particulier que l'indexation sur le SMIC a lieu en 1972 et
         # on ne peut pas, ailleur qu'ici faire comme si ce changement dépendant de l'année de la base de donnée
         # ce ne serait toutefois pas difficile à faire, il suffit de l'inscrire vraiment comme une étape de calcul
-        # 
+        #
         # En résumé, on ne fait ici qu'écrire un salref qu'on aurait pu relevé dans la législation
         # Note :Toute la série chronologique est exprimé en euros
-        
+
         # Article R351-9 du code de la sécurité sociale
         # de 1949 à 1972 -> AVTS, après jusqu'en 2014, 200 fois le smic horaire de la première année, ensuite 150 fois.
         param_long = self.P_longit
@@ -117,35 +117,35 @@ class PensionParam(object):
         smic_key = sorted(smic.keys())
         avts_key = sorted(avts.keys())
         debut_annee = '-01-01'
-        
+
         salref = dict()
         k = -1
         year = 1949 # supposer and year < self.date.year
-        while year < 1972 and year < self.date.year: 
+        while year < 1972 and year < self.date.year:
             date = str(year) + debut_annee
             while avts_key[k+1] <= date:
                 k +=1
             salref[date] = avts[avts_key[k]]/4
             if compare_destinie == True:
                 salref[date] = 1
-            year += 1           
-            
-        k = -1 
-        while year < 2014 and year < self.date.year: 
+            year += 1
+
+        k = -1
+        while year < 2014 and year < self.date.year:
             date = str(year) + debut_annee
             while smic_key[k+1] <= date:
                 k +=1
             salref[date] = 200*smic[smic_key[k]]
             year += 1
 
-        while year < 2014 and year < self.date.year: 
+        while year < 2014 and year < self.date.year:
             date = str(year) + debut_annee
             while smic_key[k+1] <= date:
                 k +=1
             salref[date] = 150*smic[smic_key[k]]
-            year += 1        
+            year += 1
 
-        self.P_longit.prive.RG.salref = salref       
+        self.P_longit.prive.RG.salref = salref
 
 
 class PensionLegislation(object):
@@ -157,10 +157,10 @@ class PensionLegislation(object):
     '''
     def __init__(self, param):
         #TODO: use all attrbutes except data in a PensionParam class
-        # example: 
-        #     duration = data.last_year - data.first_year 
+        # example:
+        #     duration = data.last_year - data.first_year
         #     self.param = PensionParam.builder(dateleg, data.info_ind, duration)
-        #  or, by anticipation: 
+        #  or, by anticipation:
         #     self.param = PensionParam.builder(dateleg, data.info_ind, duration, method)
         #  where method give how to shift legislation from on year to an other, constant_year, constant_sequence, etc?
         self.param = param
@@ -169,62 +169,62 @@ class PensionLegislation(object):
                             complementaires = [ARRCO(), AGIRC()],
                             )
         self.date = param.date
-            
-    def long_param_builder(self, duration_sim): 
-        ''' Cette fonction permet de traduire les paramètres longitudinaux en vecteur numpy 
+
+    def long_param_builder(self, duration_sim):
+        ''' Cette fonction permet de traduire les paramètres longitudinaux en vecteur numpy
         comportant une valeur par année comprise entre first_year_sim et last_year_sim '''
         yearleg = self.date.year
         P_longit = self.param.P_longit
         first_year_sim = yearleg - 1 - duration_sim
         last_year_sim = yearleg
         # TODO: trouver une méthode plus systématique qui test le 'type' du noeud et construit le long parameter qui va bien
-        for param_name in ['common.plaf_ss', 'prive.RG.revalo','common.smic_proj','common.avpf', 
+        for param_name in ['common.plaf_ss', 'prive.RG.revalo','common.smic_proj','common.avpf',
                             'prive.RG.surcote.dispositif1.dates1','prive.RG.surcote.dispositif1.dates2',
                             'prive.RG.surcote.dispositif2.dates', 'public.fp.surcote.dates',
-                            'prive.RG.salref', 'public.fp.val_point', 
+                            'prive.RG.salref', 'public.fp.val_point',
                             'prive.complementaire.agirc.maj_enf.born.dispositif0.dates', 'prive.complementaire.agirc.maj_enf.born.dispositif1.dates',
                             'prive.complementaire.arrco.maj_enf.born.dispositif0.dates', 'prive.complementaire.arrco.maj_enf.born.dispositif1.dates']:
             param_name = param_name.split('.')
             param = reduce(getattr, param_name, P_longit)
             param = build_long_values(param_long=param, first=first_year_sim, last=last_year_sim)
             setattr(eval('P_longit.' + '.'.join(param_name[:-1])), param_name[-1], param)
-            
+
 
         for regime in self.regimes['complementaires']:
             regime = regime.name
             P = getattr(P_longit.prive.complementaire, regime)
             salref_long = P.sal_ref
             salref_long = build_long_values(salref_long,
-                                        first=first_year_sim, last=last_year_sim) 
+                                        first=first_year_sim, last=last_year_sim)
             setattr(eval('P_longit.prive.complementaire.' + regime), 'sal_ref', salref_long)
             taux_cot_long = P.taux_cot_moy
             taux_cot_long = build_long_values(taux_cot_long,
                                                first=first_year_sim, last=last_year_sim)
             taux_cot_long = scales_long_baremes(baremes=taux_cot_long, scales=P_longit.common.plaf_ss)
             setattr(eval('P_longit.prive.complementaire.' + regime), 'taux_cot_moy', taux_cot_long)
-                      
+
         return P_longit
 
 
 if __name__ == '__main__':
     from pension_data import PensionData
     import datetime
-    
+
     data = DataFrame()
     table = array([ (186L, 1941.0, 2.0, 1.0, datetime.date(1941, 1, 1), 186L, 756.0, 0.0, 2.0, datetime.date(2060, 1, 1)),
        (376L, 1941.0, 1.0, 1.0, datetime.date(1941, 1, 1), 376L, 756.0, 0.0, 1.0, datetime.date(2060, 1, 1)),
        (833L, 1941.0, 3.0, 0.0, datetime.date(1941, 1, 1), 833L, 756.0, 0.0, 3.0, datetime.date(2060, 1, 1)),
        (834L, 1941.0, 3.0, 1.0, datetime.date(1941, 1, 1), 834L, 756.0, 0.0, 3.0, datetime.date(2060, 1, 1)),
-       (956L, 1941.0, 0.0, 0.0, datetime.date(1941, 1, 1), 956L, 756.0, 0.0, 0.0, datetime.date(2060, 1, 1))], 
+       (956L, 1941.0, 0.0, 0.0, datetime.date(1941, 1, 1), 956L, 756.0, 0.0, 0.0, datetime.date(2060, 1, 1))],
       dtype=[('index', '<i8'), ('t_naiss', '<f8'), ('n_enf', '<f8'), ('sexe', '<f8'), ('naiss', 'O'), ('id', '<i8'), ('agem', '<f8'), ('nb_pac', '<f8'), ('nb_born', '<f8'), ('date_liquidation', 'O')])
     info_ind = DataFrame(table)
     sali = DataFrame(0, index=info_ind.index, columns=[201301,201401])
     data = PensionData.from_arrays(sali, sali, info_ind)
-    
+
     param = PensionParam(2015, data)
     print param.param.prive.RG.prorat
     print param.param.prive.RG.prorat
-    
+
     legislation = PensionLegislation(param)
     legislation.salref_RG_builder(1000)
 
