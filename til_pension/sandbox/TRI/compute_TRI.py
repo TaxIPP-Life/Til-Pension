5d249acfdcbf93f5ec65185de665544804b57624
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2015-05-07 19:44:09 +0200

    Start transition of compute_TRI (WIP)

--- til_pension/sandbox/TRI/compute_TRI.py
+++ til_pension/sandbox/TRI/compute_TRI.py
@@ -3,7 +3,7 @@
 
 import pandas as pd
 from numpy import nan, array, around, double
-from til_pension.sandbox.compare.CONFIG_compare import pensipp_comparison_path as pensipp_data_path 
+from til_pension.sandbox.compare.CONFIG_compare import pensipp_comparison_path as pensipp_data_path
 from til_pension.simulation import PensionSimulation
 from til_pension.pension_legislation import PensionParam, PensionLegislation
 from til_pension.sandbox.compare.load_pensipp import load_pensipp_data
@@ -40,7 +40,7 @@ def TRI(pensions_contrib, val0 = 1.3):
         rate = -1
     if rate> 1:
         rate = -1
-    return 1/rate - 1 
+    return 1/rate - 1
 
 def compute_TRI(yearmin, yearmax):
     depart = dict()
@@ -53,14 +53,17 @@ def compute_TRI(yearmin, yearmax):
         param = PensionParam(yearsim, data_bounded)
         legislation = PensionLegislation(param)
         simul_til = PensionSimulation(data_bounded, legislation)
-        dates_taux_plein = simul_til.evaluate(output="dates_taux_plein")
+        regimes = [ 'RG']  # TODO:, 'agirc', 'arrco', 'FP', 'RSI']
+        for regime in regimes:
+            dates_taux_plein = simul_til.calculate("date_start_taux_plein", regime_name = regime)
+
         to_select = (dates_taux_plein == (yearsim - 1)*100 + 1) # vont partir en retraite à yearsim (=t) car ont satisfaits les conditions de taux plein à yearsim - 1 (=t-1)
         ident_depart = simul_til.data.info_ind['index'][to_select]
         ident_depart = [ident for ident in ident_depart if ident not in already_retired]
         depart[yearsim] = ident_depart
         already_retired += ident_depart
-        print "Nb of retired people for ",yearsim, len(ident_depart) 
-        
+        print "Nb of retired people for ",yearsim, len(ident_depart)
+
     all_dates = [str(year*100 + 1) for year in range(first_year_sal, yearmax)]
     regimes = [ 'RG', 'agirc', 'arrco', 'FP', 'RSI']
     nb_reg = len(regimes)
@@ -86,13 +89,13 @@ def compute_TRI(yearmin, yearmax):
             pensions_contrib.loc[cond, 'pension'] = pensions_year[reg]
             if 'sal' in cotisations_year[reg].keys() and 'pat' in cotisations_year[reg].keys():
                 pensions_contrib.loc[cond, dates_yearsim] = cotisations_year[reg]['sal'] + cotisations_year[reg]['pat']
-            else: 
-                pensions_contrib.loc[cond, dates_yearsim] = cotisations_year[reg]['tot']   
+            else:
+                pensions_contrib.loc[cond, dates_yearsim] = cotisations_year[reg]['tot']
         pensions_contrib['year_dep'][pensions_contrib['ident'].isin(ident_depart)] = yearsim
         cond = (pensions_contrib['ident'].isin(ident_depart)) & (pensions_contrib['regime'] == 'RG')
         pensions_contrib.loc[cond,'age'] = data_bounded.info_ind['agem'][data_bounded.info_ind['index'] == ident_depart] // 12
         for var in ['naiss', 'n_enf', 'findet', 'sexe']:
-                pensions_contrib.loc[cond,var] = data_bounded.info_ind[var][data_bounded.info_ind['index'] == ident_depart] 
+                pensions_contrib.loc[cond,var] = data_bounded.info_ind[var][data_bounded.info_ind['index'] == ident_depart]
         pensions_contrib.index = range(len(ident_index))
 
     for var in ['age', 'naiss', 'n_enf', 'findet', 'sexe']:
@@ -102,23 +105,23 @@ def compute_TRI(yearmin, yearmax):
     pensions_contrib.loc[:,'sexe'] = pensions_contrib.loc[:,'sexe'].fillna(0)
     pensions_contrib = pensions_contrib[~(pensions_contrib['pension'] == 0)]
     #TODO: Arbitrary for the moment -> add differential life expenctancy
-    pensions_contrib['death'] = (pensions_contrib['year_dep'] - (pensions_contrib['age'] - 60) + 22).astype(int) 
+    pensions_contrib['death'] = (pensions_contrib['year_dep'] - (pensions_contrib['age'] - 60) + 22).astype(int)
     for var in ['pension'] + [str(year*100 + 1) for year in range(first_year_sal, yearmax)]:
         pensions_contrib.loc[:, var] = around(pensions_contrib[var].astype(double),2)
     pensions_contrib.loc[:,'age'] = pensions_contrib['age'] - 1
-    
+
     pensions_contrib['TRI'] = pensions_contrib.apply(TRI, axis=1)
     return pensions_contrib
-        
+
 if __name__ == '__main__':
 
     first_year = 2009
     last_year = 2019
     result = compute_TRI(first_year, last_year)
     print result.groupby(['regime', 'sexe'])['TRI'].mean()
-    print result.groupby(['regime', 'sexe'])['TRI'].median() 
-    print result.groupby(['regime', 'sexe'])['age'].mean() 
-    print result.groupby(['regime'])['age'].median() 
-    print result.groupby(['regime', 'naiss'])['age'].mean() 
-    print result.groupby(['regime', 'age'])['pension'].mean() 
+    print result.groupby(['regime', 'sexe'])['TRI'].median()
+    print result.groupby(['regime', 'sexe'])['age'].mean()
+    print result.groupby(['regime'])['age'].median()
+    print result.groupby(['regime', 'naiss'])['age'].mean()
+    print result.groupby(['regime', 'age'])['pension'].mean()
     #result.to_csv('result.csv')
--- til_pension/sandbox/compare/CONFIG_compare_ to_rename.py
+++ til_pension/sandbox/compare/CONFIG_compare_ to_rename.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
 
-path_pension = None
 pensipp_comparison_path = None #path of  RData bases for comparison 
